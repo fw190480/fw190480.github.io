a1a4c18870c8cb23e98bf00fd2f09f4e288d1478
rahul <rkrahul8181@gmail.com>
2024-12-11 07:48:41 +0530

    fix assects and media rendering issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/rahulkr-dev.github.io/' : '',
-  basePath: isProd ? '/rahulkr-dev.github.io/' : '',
-  output: 'export'
+  // assetPrefix: isProd ? '/rahulkr-dev.github.io' : '',
+  // basePath: isProd ? '/rahulkr-dev.github.io' : '',
+  assetPrefix: "",
+  basePath: "",
+  output: "export",
 };
 
 export default nextConfig;
