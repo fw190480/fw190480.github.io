976b3d55c0b98f1b0a95ef1ddeea4bc067814475
rahul <rkrahul8181@gmail.com>
2024-12-13 17:04:58 +0530

    add constants for LinkedIn, GitHub, and resume URLs

---
+++ lib/constant.ts
@@ -0,0 +1,6 @@
+export const LINKEDIN_URL =
+  "https://www.linkedin.com/in/rahul-kumar-850b39232/";
+export const GITHUB_URL = "https://github.com/rahulkr-dev";
+export const RESUME_DRIVE_URL =
+  "https://drive.google.com/file/d/1PDkn6IqrAUmI-cT_AX7TglaZsOLdHz0Y/view?usp=sharing";
+
